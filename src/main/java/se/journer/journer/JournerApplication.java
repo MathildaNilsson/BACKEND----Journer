1b3e2ed8d8a3e9c04663bf157a6d08f1af937e9c
Mathilda <frknilsson@hotmail.com>
2022-03-23 13:27:01 +0100

    initial commit

---
+++ src/main/java/se/journer/journer/JournerApplication.java
@@ -0,0 +1,13 @@
+package se.journer.journer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/journer/journer/JournerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.journer.journer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

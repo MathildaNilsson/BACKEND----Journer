b3739f44ae17cd5194b296aadb370521b712c557
Oskar <Oskar.herdenberg@gmail.com>
2022-03-28 10:35:52 +0200

    Added getQuestion in QuestionService

--- src/main/java/se/journer/journer/service/QuestionService.java
+++ src/main/java/se/journer/journer/service/QuestionService.java
@@ -1,4 +1,19 @@
 package se.journer.journer.service;
 
+import se.journer.journer.dao.QuestionDAO;
+import se.journer.journer.question.Question;
+
+import java.util.List;
+
+
 public class QuestionService {
+
+    QuestionDAO questionDAO;
+
+    public QuestionService() {
+    }
+
+    public List<Question> getQuestion() {
+        return questionDAO.getQuestion();
+    }
 }

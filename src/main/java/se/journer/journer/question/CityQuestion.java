5a8fe7929720baa41f8fb789147bebd87d08c24f
Mathilda <frknilsson@hotmail.com>
2022-03-28 10:00:44 +0200

    added variables to questions

--- src/main/java/se/journer/journer/question/CityQuestion.java
+++ src/main/java/se/journer/journer/question/CityQuestion.java
@@ -1,4 +1,10 @@
 package se.journer.journer.question;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class CityQuestion extends Question{
+    public CityQuestion(UUID id, String question, String correctAnswer, ArrayList<String> wrongAnswer) {
+        super(id, question, correctAnswer, wrongAnswer);
+    }
 }
--- src/main/java/se/journer/journer/question/Question.java
+++ src/main/java/se/journer/journer/question/Question.java
@@ -1,4 +1,34 @@
 package se.journer.journer.question;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Question {
+    private UUID id;
+    private String Question;
+    private String correctAnswer;
+    private ArrayList<String> wrongAnswer;
+
+    public Question(UUID id, String question, String correctAnswer, ArrayList<String> wrongAnswer) {
+        this.id = id;
+        Question = question;
+        this.correctAnswer = correctAnswer;
+        this.wrongAnswer = wrongAnswer;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return Question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public ArrayList<String> getWrongAnswer() {
+        return wrongAnswer;
+    }
 }

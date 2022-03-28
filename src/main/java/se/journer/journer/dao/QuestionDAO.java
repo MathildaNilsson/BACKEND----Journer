2fc7f4e84ea26c4ff86b2798759e466ffc692b37
Oskar <Oskar.herdenberg@gmail.com>
2022-03-28 10:35:33 +0200

    Added getQuestion in questionDAO

--- src/main/java/se/journer/journer/dao/QuestionDAO.java
+++ src/main/java/se/journer/journer/dao/QuestionDAO.java
@@ -1,4 +1,21 @@
 package se.journer.journer.dao;
 
+import se.journer.journer.question.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class QuestionDAO {
+    static List<Question> questions = new ArrayList<>();
+    private static List<Question> DB =
+            new ArrayList<>(List.of(
+                    new Question(
+                            UUID.randomUUID(),
+                            "What is the capital of Assyria",
+                            "Right answer", questions)));
+
+    public List<Question> getQuestion() {
+        return DB;
+    }
 }

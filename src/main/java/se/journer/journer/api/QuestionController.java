cc438bd5ea5c430518ec030239afcc8cb26d3ff7
Oskar <Oskar.herdenberg@gmail.com>
2022-03-28 09:58:58 +0200

    Added constructor and getQuestion method

--- src/main/java/se/journer/journer/api/QuestionController.java
+++ src/main/java/se/journer/journer/api/QuestionController.java
@@ -1,4 +1,20 @@
 package se.journer.journer.api;
 
+import se.journer.journer.question.Question;
+import se.journer.journer.service.QuestionService;
+
+import java.util.List;
+
 public class QuestionController {
+
+    QuestionService questionService;
+
+    public QuestionController() {
+    }
+
+    public List<Question> getQuestion(){
+
+        return questionService.getQuestion();
+    }
+
 }

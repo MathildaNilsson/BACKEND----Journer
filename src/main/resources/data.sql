41549287597a5212ec097135bf3fbd09111ea7b3
Oskar <Oskar.herdenberg@gmail.com>
2022-03-30 12:29:33 +0200

    Added more temp questions to db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
+DELETE FROM questions;
 INSERT INTO questions(question,correct_answer,type) VALUES
-    ('Vad heter du','Oskar','cityQuestion');
+    ('Vad heter du','Oskar','cityQuestion'),
+    ('Hur gammal är du','35','jobQuestion'),
+    ('Vad gör du','Pluggar','jobQuestion'),
+    ('Vart bor du','Göteborg','cityQuestion');

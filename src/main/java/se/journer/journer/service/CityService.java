4ccdab098b05773436590b39c0aae62cb00d5e48
Mathilda <frknilsson@hotmail.com>
2022-03-30 14:59:29 +0200

    added service for city

--- src/main/java/se/journer/journer/service/CityService.java
+++ src/main/java/se/journer/journer/service/CityService.java
@@ -1,2 +1,21 @@
-package se.journer.journer.service;public class CityService {
+package se.journer.journer.service;
+
+import org.springframework.stereotype.Service;
+import se.journer.journer.dao.CityDAO;
+import se.journer.journer.models.cities.City;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    CityDAO cityDAO;
+
+    public CityService(CityDAO cityDAO) {
+        this.cityDAO = cityDAO;
+    }
+
+    public List<City> getCities() {
+        return (List<City>) cityDAO.getCities();
+    }
 }

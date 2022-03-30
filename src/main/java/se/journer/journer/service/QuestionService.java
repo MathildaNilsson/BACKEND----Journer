0e45840e6b7e7ad9e21f1d5312560605e908eece
Oskar <Oskar.herdenberg@gmail.com>
2022-03-30 12:29:05 +0200

    Added method to call for question type

--- src/main/java/se/journer/journer/service/QuestionService.java
+++ src/main/java/se/journer/journer/service/QuestionService.java
@@ -5,6 +5,7 @@ import se.journer.journer.dao.QuestionDAO;
 import se.journer.journer.models.Question;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class QuestionService {
@@ -18,4 +19,11 @@ public class QuestionService {
     public List<Question> getQuestion() {
         return (List<Question>) questionDAO.getQuestion();
     }
+
+    public List<Question> getQuestionByType(String type){
+        return getQuestion().stream()
+                .filter(Question -> type.equalsIgnoreCase(Question.getType()))
+                .collect(Collectors.toList());
+    }
+
 }

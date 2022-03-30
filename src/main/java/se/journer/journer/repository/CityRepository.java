d5ee4c6c7138512d78959c895cf24e73d2e96e3a
Mathilda <frknilsson@hotmail.com>
2022-03-30 14:58:26 +0200

    added repository for city

--- src/main/java/se/journer/journer/repository/CityRepository.java
+++ src/main/java/se/journer/journer/repository/CityRepository.java
@@ -1,2 +1,7 @@
-package se.journer.journer.repository;public interface CityRepository {
+package se.journer.journer.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.journer.journer.models.cities.City;
+
+public interface CityRepository extends CrudRepository<City, Integer> {
 }

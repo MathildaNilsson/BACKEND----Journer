5537186c0e3807f370595a16183144c7a046a9e5
Mathilda <frknilsson@hotmail.com>
2022-03-31 12:39:09 +0200

    Added 11  questions with answers to dataSQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,5 @@
 SELECT * FROM question;
 
--- Initialize Questions
--- INSERT INTO questions(question,correct_answer,type) VALUES
---     ('Vad heter du','Oskar','cityQuestion'),
---     ('Hur gammal är du','35','jobQuestion'),
---     ('Vad gör du','Pluggar','jobQuestion'),
---     ('Vart bor du','Göteborg','cityQuestion');
-
-
--- Initialize Wrong answers
--- INSERT INTO wrong_answers(answer,question) VALUES
---                                                         ('fel svar',36),
---                                                         ('fel svar2',34),
---                                                         ('fel svar3',35),
---                                                         ('fel svar4',35),
---                                                         ('fel svar5',37);
-
-
 -- Initialize Cities
 -- INSERT INTO cities(name) VALUES
 --                             ('Bangalore'),
@@ -44,34 +27,66 @@ SELECT * FROM question;
 --                              ('Singapore','Orchid parfume'),
 --                              ('Tokyo','Cherry blossom');
 
--- INSERT INTO category(category) VALUES
---                                    ('Tokyo'),
---                                    ('Bangalore'),
---                                    ('Havanna'),
---                                    ('Sao Paulo');
---
---
--- INSERT INTO question(question,category_id) VALUES
---                                                ('Fråga 1?',1),
---                                                ('Fråga 2?',2),
---                                                ('Fråga 3?',3),
---                                                ('Fråga 4?',4),
---                                                ('Fråga 5?',4),
---                                                ('Fråga 6?',2);
---
--- INSERT INTO answers(answer,right_answer,question_id) VALUES
---                                                          ('answer1',true,1),
---                                                          ('answer2',false,1),
---                                                          ('answer3',false,1),
---                                                          ('answer4',true,2),
---                                                          ('answer5',false,2),
---                                                          ('answer6',false,2),
---                                                          ('answer7',true,3),
---                                                          ('answer8',false,3),
---                                                          ('answer9',false,3),
---                                                          ('answer10',true,4),
---                                                          ('answer11',false,4),
---                                                         ('answer12',false,4);
+ --INSERT INTO category(category) VALUES
+ --                                   ('Bangalore'),
+ --                                   ('Budapest'),
+ --                                  ('Cape town'),
+ --                                   ('Chicago'),
+ --                                      ('Dubai'),
+ --                                      ('Havanna'),
+ --                                      ('Helsinki'),
+ --                                      ('Sao Paulo'),
+ --                                      ('Singapore'),
+ --                                      ('Tokyo');
+
+
+--  INSERT INTO question(question,category_id) VALUES
+--                                                 ('Hur många invånare har kapstaden?',3),
+--                                                 ('Vilket statsskick har Singapore?',9),
+--                                                 ('Vilket smeknamn brukar Bangalore gå under?',1),
+--                                                 ('Vilket år grundades Sao Paulo?',8),
+--                                                 ('I vilken delstat ligger i Chicago?',4),
+--                                                 ('Vilka år har Tokyo varit värd för Olympiska spelen?',10),
+--                                                 ('Hur många holmar finns det i Helsingfors?',7),
+--                                                 ('Vad heter världens högsta byggnad som ligger i Dubai?',5),
+--                                                 ('Vilken flod flyter genom Budapest?',2),
+--                                                 ('Vad heter Kubas största bergskedja?',6),
+--                                                 ('Vilket år grundades Dubai?',5);
+
+
+--  INSERT INTO answers(answer,right_answer,question_id) VALUES
+--                                                           ('3.7 miljoner',true,1),
+--                                                           ('1.5 miljoner',false,1),
+--                                                           ('2.8 miljoner',false,1),
+--                                                           ('Republik',true,2),
+--                                                           ('Monarki',false,2),
+--                                                           ('Diktatur',false,2),
+--                                                           ('Kuststaden',false,3),
+--                                                           ('Trädgårdsstaden',true,3),
+--                                                           ('Diamantstaden',false,3),
+--                                                           ('1395',false,4),
+--                                                           ('1467',false,4),
+--                                                           ('1554',true,4),
+--                                                           ('Illinois', true, 5),
+--                                                           ('Delaware',false,5),
+--                                                           ('Idaho',false,5),
+--                                                           ('1968  2016',false,6),
+--                                                           ('1960 & 2020',false,6),
+--                                                           ('1964 & 2021',true,6),
+--                                                           ('225',false,7),
+--                                                           ('317',true,7),
+--                                                           ('405',false,7),
+--                                                           ('Burj Khalifa',true,8),
+--                                                           ('Burj Al Arab',false,8),
+--                                                           ('Burj Dubai',false,8),                                                          ('Volga',false,9),
+--                                                           ('Rhen',false,9),
+--                                                           ('Donau',true,9),
+--                                                           ('Sierra Madre',false,10),
+--                                                           ('Sierra Maestra',true,10),
+--                                                           ('Sierra Nevada',false,10),
+--                                                           ('1833',true,11),
+--                                                           ('1798',false,11),
+--                                                           ('1878',false,11);
 
 
 

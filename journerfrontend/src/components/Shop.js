714fe8a6f0e278b030912a0698a28d2db2e94ad7
Mathilda <frknilsson@hotmail.com>
2022-04-03 18:18:58 +0200

    added shop component and useEffect in react

--- journerfrontend/src/components/Shop.js
+++ journerfrontend/src/components/Shop.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+
 const Shop = () => {
+  let [data, setdata] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/game/shop")
+      .then((response) => response.json())
+      .then((data) => setdata(data));
+  }, {});
 
   return(
     <>
-    <h1>shop</h1>
+    <h1>SHOP</h1>
+    <div id = "shop">
+      {data &&
+        data.map((item) => (
+          <tr>
+            <th scope="row">{item.name}</th>
+            <td>{item.price}:- <button>Buy</button></td>
+          </tr>
+        ))}
+    </div>
+    
     <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/`;}}>Return</button>
     </>
   )

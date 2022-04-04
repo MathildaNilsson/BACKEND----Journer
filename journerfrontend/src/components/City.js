771587a0d21212ed58f3f8f10b272bb74ab10bbe
Oskar <Oskar.herdenberg@gmail.com>
2022-04-04 11:30:00 +0200

    Added a display of the current city

--- journerfrontend/src/components/City.js
+++ journerfrontend/src/components/City.js
@@ -1,8 +1,16 @@
-const City = ({cityobj}) => {
+import { useEffect, useState } from "react";
+const City = ({}) => {
+  let [city, setCity] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/city/getcurrentcity`)
+      .then((response) => response.json())
+      .then((city) => setCity(city));
+  }, {});
 
   return(
     <>
-    <h1>{cityobj && cityobj.name}</h1>
+    <h1>{city && city.name}</h1>
     <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/`;}}>Return</button>
     </>
   )

3885353ff9ed9f332970c8519bb57c77512eac17
Oskar <Oskar.herdenberg@gmail.com>
2022-04-04 10:58:44 +0200

    Added bootstrap component and css to display player stats

--- journerfrontend/src/components/NewGameScreen.js
+++ journerfrontend/src/components/NewGameScreen.js
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { Table, Button } from "react-bootstrap";
 const NewGameScreen = () => {
+  let [player, setPlayer] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/game/getplayer")
+      .then((response) => response.json())
+      .then((player) => setPlayer(player));
+  }, {});
+
   return (
     <>
-    <h1>New game screen</h1>
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/`;}}>Return</button>
+      <h1>New game screen</h1>
+      
+      <div id="display-player-stats">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Money</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{player.name}</td>
+              <td>{player.money}:-</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <Button>Chicago</Button>
+      <br/>
+      <Button>Bangalore</Button>
+      <br/>
+      <Button>Tokyo</Button>
+      <br/>
+      <button
+        id="new-game"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = `/`;
+        }}
+      >
+        Return
+      </button>
     </>
-  )
-}
+  );
+};
 export default NewGameScreen;

457c265d6f23d2806cc91862c1de5aee3473b532
Oskar <Oskar.herdenberg@gmail.com>
2022-04-04 11:30:39 +0200

    Added button controlling to progress to new cities

--- journerfrontend/src/components/NewGameScreen.js
+++ journerfrontend/src/components/NewGameScreen.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import { Table, Button } from "react-bootstrap";
+
 const NewGameScreen = () => {
   let [player, setPlayer] = useState(null);
 
+  // Use effect to get player from database for display on screen
   useEffect(() => {
     fetch("http://localhost:8080/game/getplayer")
       .then((response) => response.json())
       .then((player) => setPlayer(player));
   }, {});
 
+  // Sets the current city in db with the incoming name cityName from button eventlistener. Copy and change fetch string to value you want to change
+  const setCurrentCity = (cityName) => {
+    fetch(`http://localhost:8080/city/setcurrentcity/${cityName}`, {
+      method: "POST",
+    }).then((response) => response.json());
+  };
+
   return (
     <>
       <h1>New game screen</h1>
-      
       <div id="display-player-stats">
         <Table striped bordered hover>
           <thead>
@@ -23,18 +31,42 @@ const NewGameScreen = () => {
           </thead>
           <tbody>
             <tr>
-              <td>{player.name}</td>
-              <td>{player.money}:-</td>
+              <td>{player && player.name}</td>
+              <td>{player && player.money}:-</td>
             </tr>
           </tbody>
         </Table>
       </div>
-      <Button>Chicago</Button>
-      <br/>
-      <Button>Bangalore</Button>
-      <br/>
-      <Button>Tokyo</Button>
-      <br/>
+      <Button
+        onClick={(e) => {
+          setCurrentCity("chicago");
+          e.preventDefault();
+          window.location.href = `/City`;
+        }}
+      >
+        Chicago
+      </Button>
+      <br />
+      <Button
+        onClick={(e) => {
+          setCurrentCity("bangalore");
+          e.preventDefault();
+          window.location.href = `/City`;
+        }}
+      >
+        Bangalore
+      </Button>
+      <br />
+      <Button
+        onClick={(e) => {
+          setCurrentCity("tokyo");
+          e.preventDefault();
+          window.location.href = `/City`;
+        }}
+      >
+        Tokyo
+      </Button>
+      <br />
       <button
         id="new-game"
         onClick={(e) => {

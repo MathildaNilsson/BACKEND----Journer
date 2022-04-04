d106e9c299677e1cc020e66ce44ff22000ddbd26
Oskar <Oskar.herdenberg@gmail.com>
2022-04-04 13:39:34 +0200

    Added bootstrap component to display player backpack

--- journerfrontend/src/components/DisplayPlayer.js
+++ journerfrontend/src/components/DisplayPlayer.js
@@ -1,33 +1,73 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Button, Offcanvas } from "react-bootstrap";
 
 const DisplayPlayer = () => {
   let [player, setPlayer] = useState(null);
+
+  //Offcanvas variables
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   // Use effect to get player from database for display on screen
   useEffect(() => {
     fetch("http://localhost:8080/game/getplayer")
       .then((response) => response.json())
       .then((player) => setPlayer(player));
   }, {});
-  return(
+  return (
     <>
-<div id="display-player-stats">
+      <div id="display-player-stats">
         <Table striped bordered hover>
           <thead>
             <tr>
               <th>Name</th>
               <th>Money</th>
+              <th>Energy</th>
+              <th>Backpack</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>{player && player.name}</td>
               <td>{player && player.money}:-</td>
+              <td>{player && player.energy}</td>
+              <td>
+                <Button variant="primary" onClick={handleShow}>
+                  Backpack
+                </Button>
+              </td>
             </tr>
           </tbody>
         </Table>
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Backpack</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            {player &&
+              player.backpack.map((list) => (
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Amount</th>
+                      <th>Name</th>
+                      <th>cost</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{}</td>
+                      <td>{list.name}</td>
+                      <td>{list.price}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              ))}
+          </Offcanvas.Body>
+        </Offcanvas>
       </div>
     </>
-  )
-}
+  );
+};
 export default DisplayPlayer;

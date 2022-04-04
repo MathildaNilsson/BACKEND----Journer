3001302a14b31838dcf6bca35e4f7c0417afa686
Oskar <Oskar.herdenberg@gmail.com>
2022-04-04 10:20:20 +0200

    Added compatibility to post to springboot using react

--- journerfrontend/src/components/Startscreen.js
+++ journerfrontend/src/components/Startscreen.js
@@ -1,45 +1,115 @@
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
-const Startscreen = ({Name}) => {
+const Startscreen = ({ Name }) => {
   let [player, setplayer] = useState(null);
   let [name, setname] = useState("");
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    this.setname({value: e.target.value})
-  }
+    /*     e.preventDefault();
+    this.setname({ value: e.target.value }); */
+  };
 
   useEffect(() => {
-    fetch(`http://localhost:8080/game/getplayer/`)
+    fetch(`http://localhost:8080/game/createplayer/test`)
       .then((response) => response.json())
       .then((player) => setplayer(player));
   }, {});
 
-  return(
-    <>
-    <form>
-     <input
-            type="text"
-            name="name"
-            placeholder="name"
-            onChange={(e) => setname(e.target.value)}
-            value={name}
-          ></input>
-    <button id = "new-game" onSubmit={submitHandler} onClick={(e) => {e.preventDefault(); window.location.href = `/newgamescreen`;}}>New Game</button>
-    <button>HighScore</button>
-
-    </form>
-
-    <div id ="temp">
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/accomondations`;}}>Accomondations</button>
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/attraction`;}}>Attraction</button>
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/backpack`;}}>Backpack</button>
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/city`;}}>City</button>
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/shop`;}}>Shop</button>
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/transportation`;}}>Transportation</button>
-    </div>
-       </>
-  )
+  const getInputValue = () =>{
+    var inputVal = document.getElementById("input-name").value;
+    fetch(`http://localhost:8080/game/createplayer/${inputVal}`, {
+     
+    // Adding method type
+    method: "POST",
+})
  
-  }
+// Converting to JSON
+.then(response => response.json())
+    
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          placeholder="name"
+          id="input-name"
+          /*           onChange={(e) => setname(e.target.value)} */
+        ></input>
+        <Button
+          variant="primary"
+          id="new-game"
+          onSubmit={submitHandler}
+          onClick={(e) => {
+            getInputValue();
+            e.preventDefault();
+            window.location.href = `/newgamescreen`;
+          }}
+        >
+          New Game
+        </Button>{" "}
+        <Button variant="primary">HighScore</Button>
+      </form>
+
+      <div id="temp">
+        <button
+          id="new-game"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = `/accomondations`;
+          }}
+        >
+          Accomondations
+        </button>
+        <button
+          id="new-game"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = `/attraction`;
+          }}
+        >
+          Attraction
+        </button>
+        <button
+          id="new-game"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = `/backpack`;
+          }}
+        >
+          Backpack
+        </button>
+        <button
+          id="new-game"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = `/city`;
+          }}
+        >
+          City
+        </button>
+        <button
+          id="new-game"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = `/shop`;
+          }}
+        >
+          Shop
+        </button>
+        <button
+          id="new-game"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = `/transportation`;
+          }}
+        >
+          Transportation
+        </button>
+      </div>
+    </>
+  );
+};
 export default Startscreen;

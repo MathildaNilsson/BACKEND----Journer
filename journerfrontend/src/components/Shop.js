7e710427c6173771bbfd6b31522d836bca4e74a7
Oskar <Oskar.herdenberg@gmail.com>
2022-04-05 11:35:45 +0200

    Added shopping functionality to shop component

--- journerfrontend/src/components/Shop.js
+++ journerfrontend/src/components/Shop.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import DisplayPlayer from "./DisplayPlayer";
 
 const Shop = () => {
   let [data, setdata] = useState(null);
@@ -9,15 +11,25 @@ const Shop = () => {
       .then((data) => setdata(data));
   }, {});
 
+  const buyItem = (item) =>{
+    
+    item = item.toLowerCase();
+    console.log(`http://localhost:8080/game/add${item}tobackpack`)
+    fetch(`http://localhost:8080/game/add${item}tobackpack`, {
+
+    method: "POST",
+})}
+
   return(
     <>
+    <DisplayPlayer />
     <h1>SHOP</h1>
     <div id = "shop">
       {data &&
         data.map((item) => (
           <tr>
             <th scope="row">{item.name}</th>
-            <td>{item.price}:- <button>Buy</button></td>
+            <td>{item.price}:- <Button onClick={() => {window.location.reload();buyItem(item.name)}}>Buy</Button></td>
           </tr>
         ))}
     </div>

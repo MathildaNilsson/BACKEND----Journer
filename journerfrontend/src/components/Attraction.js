8633344fbac34a814b9c731d60b122284ffb658d
Oskar <Oskar.herdenberg@gmail.com>
2022-04-08 13:31:20 +0200

    Trying to fix react

--- journerfrontend/src/components/Attraction.js
+++ journerfrontend/src/components/Attraction.js
@@ -1,40 +1,113 @@
 import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
+let category ="";
+
 const Attraction = (props) => {
-  let [categoryQuestion, setCategoryQuestion] = useState(null);
+  const [showQuestions, setShowQuestions] = useState(false);
+  const onClick = () => {
+    setShowQuestions(true);
+  };
+  const [showAttractions, setShowAttractions] = useState(true);
+  const onClick2 = () => {
+    setShowAttractions(false);
+  };
 
-  useEffect(() => {
-    fetch(`http://localhost:8080/question/categoryname/bangalore palace`)
+  let [question, setquestion] = useState(null);
+
+
+  /*   useEffect(() => {
+    fetch("http://localhost:8080/question/random/5")
       .then((response) => response.json())
-      .then((categoryQuestion) => setCategoryQuestion(categoryQuestion));
-  }, {});
+      .then((question) => setquestion(question));
+  }, []); */
 
-  return(
-    <>
-    <h1>attraction</h1>
-    {categoryQuestion && categoryQuestion.map((question) => <div>
-      {question.question}
-    </div>)}
+    useEffect(() => {
+      console.log(category);
+      fetch(`http://localhost:8080/question/categoryname/bangalore palace`)
+        .then((response) => response.json())
+        .then((question) => setquestion(question));
+    },[]);
+  
 
+  const checkAnswer = (answer) => {
+    if (answer === true) {
+      alert("rätt");
+    } else {
+      alert("fel");
+    }
+  };
 
-    <Button variant="primary">
+  const Questions = (props) => (
+    <div>
+      <h1>{question[0].question}</h1>
+
+      <span>
+        {question[0].answerList.map((answer) => (
+          <Button onClick={(e) => checkAnswer(answer.rightAnswer)}>
+            {answer.answer}
+          </Button>
+        ))}
+      </span>
+    </div>
+  );
+
+  const AttractionButtons = (props) => (
+    <div>
+      <Button
+        variant="primary"
+        onClick={(e) => {
+          category = (props.city.attractionList[0].name)
+          onClick();
+          onClick2();
+        }}
+      >
         {props.city.attractionList[0].name}
       </Button>
 
-      <Button variant="primary" >
-      {props.city.attractionList[1].name}
+      <Button
+        variant="primary"
+        onClick={(e) => {
+          category = (props.city.attractionList[1].name)
+          onClick();
+          onClick2();
+        }}
+      >
+        {props.city.attractionList[1].name}
       </Button>
 
-      <Button variant="primary" >
-      {props.city.attractionList[2].name}
+      <Button
+        variant="primary"
+        onClick={(e) => {
+          category = (props.city.attractionList[2].name)
+          onClick();
+          onClick2();
+        }}
+      >
+        {props.city.attractionList[2].name}
       </Button>
+    </div>
+  );
 
+  return (
+    <>
+      <h1>attraction</h1>
+      <div>
+        {showQuestions ? <Questions city = {props.city}/> : null}
+        {showAttractions ? <AttractionButtons city = {props.city}/> : null}
+      </div>
+      <div></div>
 
-
-
-    <button id = "new-game" onClick={(e) => {e.preventDefault(); window.location.href = `/`;}}>Return</button>
+      <button
+        id="new-game"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = `/`;
+        }}
+      >
+        Return
+      </button>
     </>
-  )
-}
+  );
+};
 export default Attraction;

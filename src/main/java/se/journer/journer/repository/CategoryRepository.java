8648a02b90ed487c9dcc11c03baa6d2b4cfdd718
Mathilda <frknilsson@hotmail.com>
2022-04-08 13:55:28 +0200

    added category repository to get categories from DB

--- src/main/java/se/journer/journer/repository/CategoryRepository.java
+++ src/main/java/se/journer/journer/repository/CategoryRepository.java
@@ -1,2 +1,8 @@
-package se.journer.journer.repository;public interface CategoryRepository {
+package se.journer.journer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import se.journer.journer.models.questions.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
 }

c71d6ed94e05a982b0fe71856c498a1d8b8890b0
Oskar <Oskar.herdenberg@gmail.com>
2022-04-13 22:49:09 +0200

    Added functionality to frontend to save highscore

--- journerfrontend/src/components/EndGameScreen.js
+++ journerfrontend/src/components/EndGameScreen.js
@@ -1,11 +1,62 @@
+import { Button } from "bootstrap";
 import { Component } from "react";
 class EndGameScreen extends Component {
   render() {
-      return (
-          <div className='container'>
-            <h1>GAME OVER</h1>
-          </div>
-      );
+    const { players: player } = this.state;
+
+    const test = () => {
+      fetch(`/highscore`, {
+        method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: player.name,
+        money: player.money,
+        completedCities : player.completedCities})
+      }).then((response) => response.json());
+    };
+
+/*     const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: player.name,
+        money: player.money,
+        completedCities : player.completedCities/2})
+  };
+  fetch('/highstgcore', requestOptions)
+      .then(response => response.json())
+      .then(data => this.setState({ postId: data.id }),[]); */
+
+
+    return (
+      <div className="container">
+        <h1>GAME OVER</h1>
+        <h2>Namn: {player.name}</h2>
+        <h2>Pengar: {player.money}</h2>
+        <h2>Klarade städer: {player.completedCities}</h2>
+        <div>
+          {player.backpack?.map((item) => (
+            <div>{item.name} </div>
+          ))}
+        </div>
+        <button onClick={(e) => 
+          test()}> spara Highscore
+          </button>
+      </div>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { players: [] };
+  }
+
+  componentDidMount() {
+    fetch("/game/countcities/")
+      .then((response) => response.json())
+      .then((count) => this.setState({ counts: count }));
+    fetch("/game/getplayer/")
+      .then((response) => response.json())
+      .then((player) => this.setState({ players: player }));
   }
 }
+
 export default EndGameScreen;

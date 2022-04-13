b10e0529fc4b7d704cae206df565dd6a7f5ad0e2
Oskar <Oskar.herdenberg@gmail.com>
2022-04-13 18:22:09 +0200

    restructured highscore to be more inline with current vision

--- src/main/java/se/journer/journer/models/highscore/Highscore.java
+++ src/main/java/se/journer/journer/models/highscore/Highscore.java
@@ -2,13 +2,47 @@ package se.journer.journer.models.highscore;
 
 import se.journer.journer.models.player.Player;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "highscore")
 public class Highscore {
-    List<Player> playerList;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private int money;
+    private int completedCities;
 
     public Highscore() {
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getCompletedCities() {
+        return completedCities;
+    }
+
+    public void setCompletedCities(int completedCities) {
+        this.completedCities = completedCities;
+    }
 }
